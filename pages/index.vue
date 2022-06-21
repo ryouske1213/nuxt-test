2c941c99262315eab0c49a6fb85a1e4e1c2d5208
ryouske1213 <san257718@yahoo.com.tw>
2022-06-21 13:43:31 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  {{$store.state.store.count}}
+  <p>========================</p>
+  <div>
+    {{$store.state.game.count}}
+  </div>
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import {mapState} from 'vuex'
+
+export default Vue.extend({
+
+  computed: mapState(['count'])
+})
+</script>
+
+q
---
+++ store/store.js
@@ -0,0 +1,9 @@
+export const state = () => ({
+    count: 999
+})
+
+// export const mutations ={
+//     add(){
+//         state.count++
+//     }
+// }
